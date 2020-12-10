707ba61e218cf42fa8d96a036c210c31624198be
HamzaEnseirb <56870021+HamzaEnseirb@users.noreply.github.com>
2020-12-10 10:55:35 +0100

    Update main.m

--- main.m
+++ main.m
@@ -1,12 +1,17 @@
-clear, clc, close all;
+clear, clc; close all;
 
-img = imread('index.png');
+seuil = 161;
+img = imread('index2.jpg');
 img=img(:,:,1);
-img = imread('cameraman.tif');
+%img = imread('cameraman.tif');
 figure, imshow(img);
 colormap('gray');
 [x,y] = ginput(2);
 
 line(x,y);
 [X,Y,signature]=premiere_signature(img,x,y);
+
+signature = binarisation(signature,seuil);
+
 figure, plot(signature);
+title("Signature binarisée")
